c97ce05c0c7c8aba07b305a1f2cbe714a17a6481
Sebax666 <johans00133@gmail.com>
2026-06-12 22:02:31 -0500

    modificaciones

--- GAMEDEX/settings.py
+++ GAMEDEX/settings.py
@@ -123,12 +123,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # -------------------------------------------------
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-if os.environ.get('CREATE_SUPERUSER') == 'True':
-    username = os.environ.get('SU_USERNAME')
-    email = os.environ.get('SU_EMAIL')
-    password = os.environ.get('SU_PASSWORD')
-
-    if username and password and not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
